e02af113093a03de2c05c0ccb8973d9ff705aea5
Pierre <10262302+Hugo54x@users.noreply.github.com>
2020-03-06 22:42:21 +0100

    Added Tensorboard Port 6006

--- Docker_Setup_JupyterLab_Tensorflow.ps1
+++ Docker_Setup_JupyterLab_Tensorflow.ps1
@@ -1,2 +1,2 @@
-docker run -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -e GRANT_SUDO=yes -v ${PWD}:/home/jovyan/work jupyter/tensorflow-notebook start-notebook.sh --NotebookApp.password='' --NotebookApp.token=''
+docker run -p 8888:8888 -p 6006:6006 -e JUPYTER_ENABLE_LAB=yes -e GRANT_SUDO=yes -v ${PWD}:/home/jovyan/work jupyter/tensorflow-notebook start-notebook.sh --NotebookApp.password='' --NotebookApp.token=''
 exit
